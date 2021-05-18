7e587037b8712df26a774b0e7b83e440e4e117ef
Anum23432 <anumaneesshah@gmail.com>
2021-05-18 21:02:15 +0500

    JS Basics

---
+++ basics.js
@@ -0,0 +1,10 @@
+function chooseWebDevelopment() {
+
+    alert('Hello, this is web development course.');
+}
+function chooseFreelancing() {
+    alert('Hello, this is free lancing course.');
+}
+function chooseGraphicdesigning() {
+    alert('Hello, this is graphic designing course.')
+}
